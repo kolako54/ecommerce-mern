10af630a1060cbae28fcb96f8db1bb201c2a2be2
unknown <parhamwolf051@gmail.com>
2021-11-14 20:43:27 +0330

    f1: connect local mongo to mongo atlas remote

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './navbar'
+import Notify from './Notify'
 
 export default function Layout({children}) {
     return (
         <div className="container">
             <Navbar/>
+            <Notify />
             {children}
         </div>
     )
--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    "BASE_URL": true,
+    "BASE_URL": 'http://localhost:3000',
     "MONGO_URL": "mongodb+srv://elio:L9Jm0IvyCqfZrp0l@cluster0.dogac.mongodb.net/social_media?retryWrites=true&w=majority"
   }
 }
