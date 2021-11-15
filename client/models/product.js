bb65ef78403a0f03776197b05c2c833133db323f
unknown <parhamwolf051@gmail.com>
2021-11-16 01:28:49 +0330

    add product model

---
+++ client/models/product.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose'
+
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    price: {
+        type: Number,
+        requird: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    images: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    checked: {
+        type: Boolean,
+        default: false,
+    },
+    inStock: {
+        type: Number,
+        default: 0
+    },
+    sold: {
+        type: Number,
+        default: 0,
+    },
+}, {
+    timestamps: true
+})
+
+const ProductModel = mongoose.models.product || mongoose.model('product', ProductSchema);
+export default ProductModel;
